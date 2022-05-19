51cbb1d3a9d129f8d9e31e9fbbc921cc40ae64a8
qiyx <51813543+qiyx2019@users.noreply.github.com>
2022-05-19 15:51:18 +0800

    useTransition

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,4 +1,11 @@
-import { useCallback, useMemo, useState, useEffect, memo } from "react";
+import {
+  useCallback,
+  useMemo,
+  useState,
+  useEffect,
+  memo,
+  useTransition,
+} from "react";
 import * as React from "react";
 export default () => {
   const [count, setCount] = useState(null);
@@ -12,7 +19,6 @@ export default () => {
     setCount((count) => count + 1);
   }, []);
   const Props = {
-   
     handleChange1,
   };
   console.log("demo渲染了");
@@ -23,10 +29,12 @@ export default () => {
       <input onChange={handleChange} />
       <Child {...Props} />
       <Child2 />
+      <br />
+      <Child3 />
     </div>
   );
 };
-const Child = memo(({ data, handleChange1 }:any) => {
+const Child = memo(({ data, handleChange1 }: any) => {
   console.log("child渲染了");
   return (
     <div>
@@ -35,9 +43,24 @@ const Child = memo(({ data, handleChange1 }:any) => {
     </div>
   );
 });
-const Child2 = memo(()=> {
-    console.log("child2渲染了");
-    return <>
-    child2
+const Child2 = memo(() => {
+  console.log("child2渲染了");
+  return <>child2</>;
+});
+const Child3 = memo(() => {
+  console.log("Chilid3渲染了");
+  const [isPending, startTransition] = useTransition();
+  const [name, setName] = useState("");
+  const handleClick = useCallback(() => {
+    startTransition(() => {
+      setName("child3");
+    });
+  }, [name]);
+  return (
+    <>
+      {name}
+      {isPending && <div>Loading</div>}
+      <button onClick={handleClick}>clickme</button>
     </>
-})
+  );
+});
